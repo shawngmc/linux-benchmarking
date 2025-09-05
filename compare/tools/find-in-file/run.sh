374a0ba9f6502f7f68113dcfd50c71e6877b9df3
Shawn McNaughton <shawn@containment-construct.local.hitoma.com>
2025-09-05 13:30:24 -0400

    more cleanup

--- compare/tools/find-in-file/run.sh
+++ compare/tools/find-in-file/run.sh
@@ -3,8 +3,9 @@ IMAGENAME=localhost/shawngmc/find-in-files:latest
 CONTAINERNAME=bench-find-in-files
 podman rmi -i "${IMAGENAME}"
 podman build -t "${IMAGENAME}" .
-podman create --name ${CONTAINERNAME} ${IMAGENAME}
-podman start -a ${CONTAINERNAME}
-podman cp ${CONTAINERNAME}:/root/results.json ./
-podman cp ${CONTAINERNAME}:/root/results.MD ./
-podman rm ${CONTAINERNAME}
+podman create --name "${CONTAINERNAME}" "${IMAGENAME}"
+podman start -a "${CONTAINERNAME}"
+podman cp "${CONTAINERNAME}:/root/results.json" ./
+podman cp "${CONTAINERNAME}:/root/results.MD" ./
+podman rm "${CONTAINERNAME}"
+podman rmi "${IMAGENAME}"
